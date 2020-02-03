00022bb4ee6f58af1eb04dd6efde53988d97022f
Aditya Malte <aditya.malte@gmail.com>
2020-02-03 18:43:41 +0530

    Update utils_text.py

--- utils_text.py
+++ utils_text.py
@@ -20,76 +20,79 @@ class PreProcess:
       import emoji
 
   def preprocess(self, text=""):
-      processed_text = text
-      
-      #separate the external url 
-      if(self.sep_url):
-        url = None
-        processed_text, _, url = text.partition("…")
-        if(url!=''):
-            url = "".join(url.split())
-            url = "https://t.co/" + url.rsplit('/', 1)[-1]
+      try:
+        processed_text = text[:]
+
+        #separate the external url 
+        if(self.sep_url):
+          url = None
+          processed_text, _, url = text.partition("…")
+          if(url!=''):
+              url = "".join(url.split())
+              url = "https://t.co/" + url.rsplit('/', 1)[-1]
+          else:
+              url=None
+
+        #remove urls in text
+        if(self.remove_url):
+          processed_text = re.sub("http\S+", "", processed_text, flags=re.MULTILINE)
+
+
+        #handle hashtags and usernames
+        if(self.remove_hashtag):
+          processed_text = re.sub("#", "", processed_text)
+        if(self.remove_usertag):
+          processed_text = re.sub("@", "", processed_text)
+
+
+
+        #remove repeated punctuations
+        if(self.remove_punct):
+            for punctuation in string.punctuation:
+                while True:
+                    replaced =  processed_text.replace(punctuation * 2, punctuation)
+                    if replaced == processed_text:
+                        break
+                    processed_text = replaced
+
+
+
+        #tokenize punctuations
+        """
+        for punctuation in string.punctuation:
+            processed_text =  processed_text.replace(punctuation, " " + punctuation+ " ")
+        """ 
+
+
+        #remove numbers
+        if(self.remove_no):
+          processed_text = re.sub("\d+", "", processed_text)
+
+
+
+        #convert emojis
+        if(self.convert_emoji):
+          processed_text = emoji.demojize(processed_text)
+
+
+
+        #convert multiple whitespaces to single
+        #detect newline and replace with random string
+        processed_text = processed_text.replace("\n", "QSDWDSrfefafawecsd")
+        processed_text = re.sub("\s\s+", " ", processed_text)
+        #replace again with newline
+        processed_text = processed_text.replace("QSDWDSrfefafawecsd", "\n")
+
+        #Convert to lower case
+        if(self.lowercase):   
+          processed_text = processed_text.lower()
+
+        if(processed_text == ""):
+          processed_text = "blank"
+
+        if self.sep_url:
+          return (processed_text, url)
         else:
-            url=None
-      
-      #remove urls in text
-      if(self.remove_url):
-        processed_text = re.sub("http\S+", "", processed_text, flags=re.MULTILINE)
-    
-
-      #handle hashtags and usernames
-      if(self.remove_hashtag):
-        processed_text = re.sub("#", "", processed_text)
-      if(self.remove_usertag):
-        processed_text = re.sub("@", "", processed_text)
-      
-      
-      
-      #remove repeated punctuations
-      if(self.remove_punct):
-          for punctuation in string.punctuation:
-              while True:
-                  replaced =  processed_text.replace(punctuation * 2, punctuation)
-                  if replaced == processed_text:
-                      break
-                  processed_text = replaced
-
-      
-      
-      #tokenize punctuations
-      """
-      for punctuation in string.punctuation:
-          processed_text =  processed_text.replace(punctuation, " " + punctuation+ " ")
-      """ 
-      
-      
-      #remove numbers
-      if(self.remove_no):
-        processed_text = re.sub("\d+", "", processed_text)
-      
-      
-      
-      #convert emojis
-      if(self.convert_emoji):
-        processed_text = emoji.demojize(processed_text)
-      
-      
-      
-      #convert multiple whitespaces to single
-      #detect newline and replace with random string
-      processed_text = processed_text.replace("\n", "QSDWDSrfefafawecsd")
-      processed_text = re.sub("\s\s+", " ", processed_text)
-      #replace again with newline
-      processed_text = processed_text.replace("QSDWDSrfefafawecsd", "\n")
-      
-      #Convert to lower case
-      if(self.lowercase):   
-        processed_text = processed_text.lower()
-      
-      if(processed_text == ""):
-        processed_text = "blank"
-      
-      if self.sep_url:
-        return (processed_text, url)
-      else:
-        return processed_text
+          return processed_text
+      catch:
+        print("Error at: ", text)
